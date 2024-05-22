2361d40026af3f0773e9e9a9150a4d09c0c99148
Arielga <81482233+a7xpadd3r@users.noreply.github.com>
2024-05-22 18:02:24 -0300

    Initial FSM
    Created Finite State Machine basics.

---
+++ ProgSisJuegos/Assets/Scripts/AI/FiniteStateMachine.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiniteStateMachine<T>
+{
+    IState<T> _current;
+
+    public FiniteStateMachine() { }
+    public FiniteStateMachine(IState<T> initialState)
+    {
+        SetInitialState(initialState);
+    }
+
+    public void SetInitialState(IState<T> initialState) 
+    {
+        _current = initialState;
+    }
+
+    public void OnUpdate()
+    {
+        if (_current != null ) _current.Execute();
+    }
+
+    public void OnLateExecute()
+    {
+        if (_current != null ) _current.LateExecute();
+    }
+
+    public void Transition(T input)
+    { 
+        IState<T> newState = _current.GetTransition(input);
+
+        if (newState != null ) 
+        {
+            _current.Sleep();
+            _current = newState;
+            _current.SetFSM = this;
+            _current.Enter();
+        }
+    }
+
+    public IState<T> CurrentState => _current;
+
+}
